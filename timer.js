8c6462d5c4575ad3a8212f2aaec8333637d2cafc
Anjana Iyer <anjanai.yahoo@gmail.com>
2020-10-21 14:35:29 -0700

    Add tihai_timer

---
+++ timer.js
@@ -0,0 +1,35 @@
+var n = 0;
+var td_array ;
+var beep = new Audio('https://www.soundjay.com/button/beep-08b.mp3');;
+var timeouts = [];
+var stopped = false;
+
+function stopTihai() {
+    for (var i=0; i<timeouts.length; i++) {
+	clearTimeout(timeouts[i]);
+    }
+    stopped = true;
+}
+
+function startTihai(table_num) {
+    stopTihai();
+    stopped = false;
+    n = 0;
+    let table = "#table" + table_num + " tbody tr td";
+    let bpm = $("#bpm"+table_num).val();
+
+    // 60,000 / bpm = Ms
+    
+    let time = 60000/bpm;
+    $(table).removeClass("mark");
+    td_array = $(table);;
+    colorNext(time);
+}
+
+function colorNext(time) {
+    if (stopped) return;
+    td_array.eq(n++).addClass("mark");
+    if (n >= td_array.length) return;
+    setTimeout(colorNext.bind(null, time), time);
+    beep.play(); 
+}
